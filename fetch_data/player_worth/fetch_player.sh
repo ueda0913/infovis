e13f4174673ea7c23459684dff633df03eaeff8a
Tatsuya Maseki <tatsu.mase@icloud.com>
2022-12-08 18:57:06 +0900

    add: fetch_worthで市場価値、年齢、西暦の配列を取れるようになった

--- fetch_data/player_worth/fetch_player.sh
+++ fetch_data/player_worth/fetch_player.sh
@@ -6,3 +6,8 @@ fi
 
 # url例: https://www.transfermarkt.jp/england/startseite/verein/3299
 data=`curl $1 | grep -e "ゴールキーパー" -e "ディフェンダー" -e "ミッドフィールダー" -e "フォワード" | grep title`
+
+python3 -c "import test; test.fetch_player_url($data)"
+
+
+echo $data
