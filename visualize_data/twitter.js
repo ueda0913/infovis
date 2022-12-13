8ca702de5412a0630efdfeb7461e0109aa2d3d1e
ueda0913 <yudy.u.1205.kaijo.noa@gmail.com>
2022-12-13 20:59:03 +0900

    fix: 市場価値を円の大きさから軸にし、ポジションを色で分けた

--- visualize_data/twitter.js
+++ visualize_data/twitter.js
@@ -1,13 +1,3 @@
-var width = 630;
-var height = 250;
-// var Twitter = d3
-// .select("#Twitter") //ここをid名に変える#を前につける
-// .append("div")
-// .append("svg")
-// .attr("width", width)
-// .attr("height", height);
-
-var tooltip = d3.select("body").append("div").attr("class", "tooltip");
 var svgImgs = Twitter
     .append("image")
     .attr("xlink:href", "../fetch_data/wc-4.png")
