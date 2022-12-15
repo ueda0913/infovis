177f8935f1f5d7f891fbcacf194cb0528e26c553
Tatsuya Maseki <tatsu.mase@icloud.com>
2022-12-15 11:31:22 +0900

    不要となったファイルを消去した

--- fetch_data/player_worth/fetch_worth.py
+++
@@ -1,8 +0,0 @@
-#%%
-import sys
-import json
-for line in sys.stdin.readlines():
-    data = line.rstrip().split(',')
-    for i in range(len(data)):
-        data[i] = int(data[i])
-    print(data)
