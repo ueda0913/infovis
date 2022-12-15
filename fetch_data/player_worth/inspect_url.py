cf3867839e5e7a9ee1143429863c50d90ab1cc6b
Tatsuya Maseki <tatsu.mase@icloud.com>
2022-12-15 14:00:15 +0900

    2014年の代表選手のうち、2014年のデータがないことによるエラーが発生したことがわかった

--- fetch_data/player_worth/inspect_url.py
+++ fetch_data/player_worth/inspect_url.py
@@ -7,17 +7,23 @@ import re
 
 header = {"User-Agent": "curl/7.85.0"}
 url_ok = 'https://www.transfermarkt.jp/yuya-kubo/profil/spieler/186260'
-url_ng = 'https://www.transfermarkt.jp/keisuke-honda/profil/spieler/66521'
+# url_ng = 'https://www.transfermarkt.jp/keisuke-honda/profil/spieler/66521'
+url_ng2 = 'https://www.transfermarkt.jp/nick-rimando/profil/spieler/39434'
 content_ok = requests.get(url_ok, headers=header).text
-content_ng = requests.get(url_ng, headers=header).text
+# content_ng = requests.get(url_ng, headers=header).text
+content_ng2 = requests.get(url_ng2, headers=header).text
+#%%
+print(content_ng2)
 #%%
 info_included_ok =  [line.rstrip() for line in content_ok.split('\n') if 'series' in line][-2]
-info_included_ng =  [line.rstrip() for line in content_ng.split('\n') if 'series' in line][-2]
+# info_included_ng =  [line.rstrip() for line in content_ng.split('\n') if 'series' in line][-2]
+info_included_ng2 =  [line.rstrip() for line in content_ng2.split('\n') if 'series' in line][-2]
 print(info_included_ok)
 print('******')
-print(info_included_ng)
+# print(info_included_ng)
+print(info_included_ng2)
 #%%
-info_included = info_included_ng
+info_included = info_included_ng2
 info = [line for line in info_included.split('[')[5].split(']')[0].split('{') if 'verein' in line]
 print(info)
 #%%
