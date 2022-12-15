16ba6db085d27dcb2461ccaf3f3cc23564c6c334
Tatsuya Maseki <tatsu.mase@icloud.com>
2022-12-15 16:52:15 +0900

    add: データが存在しないものについてはnodataplayer[year].csvに格納するように

--- fetch_data/player_worth/get_worth_remain/get_worth_remain2013.py
+++ fetch_data/player_worth/get_worth_remain/get_worth_remain2013.py
@@ -11,6 +11,7 @@ header = {"User-Agent": "curl/7.85.0"}
 player_csv_path = 'csv/playerinfo_remain'
 worth_csv_path = 'csv/playerworth_remain'
 error_csv_path = 'csv/errorplayer_ramain'
+nodata_csv_path = 'csv/nodataplayer_remain'
 
 p = re.compile(r'\'y\':(\d+).*age\':\'(\d+).*datum_mw\':\'(\d+)')
 
@@ -25,22 +26,28 @@ def main():
             csvwriter.writerow(['name', 'country', 'position', 'image', 'worth', 'age'])
             with open(error_csv_path + year + '.csv', 'w', encoding='utf-8' , newline='') as h:
                 csvwriter_error = csv.writer(h)
-                for row in csvreader:
-                    time.sleep(2) #2秒毎にスクレイピング
-                    worth_info = get_worth(row[-1], year)
-                    if worth_info == 0:
-                        csvwriter_error.writerow(row)
-                    else:
-                        row.pop(-1)
-                        csvwriter.writerow(row+worth_info)
-                    print(row[0])
-                    count += 1
-                    if count % 10 == 0:
-                        print(count)
+                with open(nodata_csv_path + year + '.csv', 'w', encoding = 'utf-8', newline ='') as i:
+                    csvwriter_nodata = csv.writer(i)
+                    for row in csvreader:
+                        time.sleep(2) #2秒毎にスクレイピング
+                        worth_info = get_worth(row[-1], year)
+                        if worth_info == -1:
+                            csvwriter_nodata.writerow(row)
+                        elif worth_info == 0:
+                            csvwriter_error.writerow(row)
+                        else:
+                            row.pop(-1)
+                            csvwriter.writerow(row+worth_info)
+                        print(row[0])
+                        count += 1
+                        if count % 10 == 0:
+                            print(count)
 
 def get_worth(url,year):
     res = requests.get(url, headers=header)
     content = res.text
+    if len([line.rstrip() for line in content.split('\n') if 'series' in line]) < 2:
+        return -1
     info_included =  [line.rstrip() for line in content.split('\n') if 'series' in line][-2]
     # print(info_included)
     info = [line for line in info_included.split('[')[5].split(']')[0].split('{') if 'verein' in line]
