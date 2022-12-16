e0df65a2ce94be33633f3ab40a7988a99d65387c
ueda0913 <yudy.u.1205.kaijo.noa@gmail.com>
2022-12-17 00:40:19 +0900

    add: wordCloud,レイアウトを少し変更

--- visualize_data/twitter.js
+++ visualize_data/twitter.js
@@ -1,7 +1,7 @@
 var svgImgs = Twitter
     .append("image")
     .attr("xlink:href", `../fetch_data/TwitterData/wordCloud/wc-${nation}-${year}.png`)
-    .attr("width", 350)
-    .attr("height", 230)
-    .attr("y", 10)
+    .attr("width", 480)
+    .attr("height", 240)
+    .attr("y", 5)
     .attr("x", 10);
